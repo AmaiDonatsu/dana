aae3a5f61bff85f330309f9c4537c7e46c574f87
Amai Tsurai Donatsu <121212122+AmaiDonatsu@users.noreply.github.com>
2026-01-02 01:59:47 -0600

    feat: migrate Expo configuration from app.json to app.config.js and update eas.json to directly reference google-services.json.

---
+++ app.config.js
@@ -0,0 +1,36 @@
+export default {
+  expo: {
+    name: "dana",
+    slug: "dana",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: false,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: "com.nissunay.dana",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./google-services.json",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      eas: {
+        projectId: "350c31be-e756-416f-828d-23bd6f055fad"
+      }
+    }
+  }
+}
